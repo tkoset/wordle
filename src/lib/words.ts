d35b0acde970bd1b086fd51e21e97aa458297288
tkoset <cmckr88@gmail.com>
2022-01-12 22:28:34 +0300

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -9,10 +9,6 @@ export const isWinningWord = (word: string) => {
 };
 
 export const getWordOfDay = () => {
-  // January 1, 2022 Game Epoch
-  const epochMs = 1641013200000;
-  const now = Date.now();
-  const msInDay = 86400000;
   const index = Math.floor(Math.random() * 5507);
   
   return WORDS[index].toUpperCase();
