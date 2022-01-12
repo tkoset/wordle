15f338d55c07ed14164e518cf678514d531a379e
tkoset <cmckr88@gmail.com>
2022-01-13 01:09:18 +0300

    Update WinModal.tsx

--- src/components/modals/WinModal.tsx
+++ src/components/modals/WinModal.tsx
@@ -9,10 +9,6 @@ type Props = {
   guesses: string[];
 };
 
-function refreshPage() {
-    window.location.reload(false);
-  }
->
   
 export const WinModal = ({ isOpen, handleClose, guesses }: Props) => {
   return (
