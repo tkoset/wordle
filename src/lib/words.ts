a46631ff9dee54ae4e1101570cf300fe8b0aeaeb
tkoset <cmckr88@gmail.com>
2022-01-12 22:23:30 +0300

    Update words.ts

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -13,7 +13,7 @@ export const getWordOfDay = () => {
   const epochMs = 1641013200000;
   const now = Date.now();
   const msInDay = 86400000;
-  const index = Math.floor((now - epochMs) / msInDay);
+  const index = Math.floor(Math.random() * 5507);;
   
   return WORDS[index].toUpperCase();
 };
