b818d8c45928ecc44aa7f7b655cb7de5f463ce33
tkoset <cmckr88@gmail.com>
2022-01-12 23:39:10 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
         isOpen={isGameLost}
       />
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold">Not Wordle</h1>
+        <h1 className="text-xl grow font-bold">Beş Harf</h1>
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
@@ -101,7 +101,7 @@ function App() {
         className="mx-auto mt-8 flex items-center px-2.5 py-1.5 border border-transparent text-xs font-medium rounded text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         onClick={() => setIsAboutModalOpen(true)}
       >
-        About this game
+        Hakkında
       </button>
     </div>
   );
