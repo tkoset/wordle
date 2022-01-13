a5e233b62caeaf8b27bfb8fb0beffef4abd92284
tkoset <cmckr88@gmail.com>
2022-01-13 17:48:17 +0300

    Update Key.tsx

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -21,8 +21,8 @@ export const Key = ({
   const classes = classnames(
     "flex items-center justify-center rounded mx-0.5 text-xs font-bold cursor-pointer",
     {
-      "bg-gray-300 hover:bg-gray-400 active:bg-gray-500": !status,
-      "bg-gray-500 text-white": status === "absent",
+      "bg-gray-200 hover:bg-gray-300 active:bg-gray-400": !status,
+      "bg-gray-400 text-white": status === "absent",
       "bg-green-500 hover:bg-green-600 active:bg-green-700 text-white":
         status === "correct",
       "bg-yellow-400 hover:bg-yellow-500 active:bg-yellow-600 text-white":
