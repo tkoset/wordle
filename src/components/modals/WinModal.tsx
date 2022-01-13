acf33fdb56ea116026dca100147ef3f7d44fc0df
tkoset <cmckr88@gmail.com>
2022-01-14 00:53:00 +0300

    Update WinModal.tsx

--- src/components/modals/WinModal.tsx
+++ src/components/modals/WinModal.tsx
@@ -46,7 +46,7 @@ export const WinModal = ({ isOpen, handleClose, guesses }: Props) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div id="refreshwrap" className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
               <div>
                 <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
                   <CheckIcon
@@ -68,7 +68,7 @@ export const WinModal = ({ isOpen, handleClose, guesses }: Props) => {
                 </div>
               </div>
               <div className="mt-5 sm:mt-6">
-                <button id="refresh"
+                <button
                   type="button"
                   className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                   onClick={handleClose}
