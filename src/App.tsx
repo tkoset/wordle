c457bce11e75ef5ea1e50148c2066ffc32765dca
tkoset <cmckr88@gmail.com>
2022-01-13 19:21:54 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ function App() {
         isOpen={isGameLost}
       />
       <div className="flex w-80 mx-auto items-center mb-8">
-        <h1 className="text-xl grow font-bold">Beş Harf</h1>
+        <h1 className="text-xl grow font-bold"></h1>
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
