e438505f9807113bb177d8ce97c294959300fb1a
tkoset <cmckr88@gmail.com>
2022-01-13 18:06:52 +0300

    Update Alert.tsx

--- src/components/alerts/Alert.tsx
+++ src/components/alerts/Alert.tsx
@@ -11,10 +11,11 @@ export const Alert = ({ isOpen, message }: Props) => {
     <Transition
       show={isOpen}
       as={Fragment}
-      enter="ease-out duration-600 transition"
+      enter="ease-out duration-300 transition"
       enterFrom="opacity-0"
       enterTo="opacity-100"
-      leave="transition ease-in duration-200"
+      duration="1200"
+      leave="transition ease-in duration-100"
       leaveFrom="opacity-100"
       leaveTo="opacity-0"
     >
