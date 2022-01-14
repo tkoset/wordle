fdb7902c09094b3768988dab2db72d4839b13408
tkoset <cmckr88@gmail.com>
2022-01-14 04:09:38 +0300

    Update WinModal.tsx

--- src/components/modals/WinModal.tsx
+++ src/components/modals/WinModal.tsx
@@ -68,13 +68,13 @@ export const WinModal = ({ isOpen, handleClose, guesses }: Props) => {
                 </div>
               </div>
               <div className="mt-5 sm:mt-6">
-                <button
+                <burton
                   type="button"
                   className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                   onClick={handleClose}
                 >
                   DEVAM
-                </button>
+                </burton>
               </div>
             </div>
           </Transition.Child>
