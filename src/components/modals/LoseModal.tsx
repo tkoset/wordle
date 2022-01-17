4283aa11e947c31289d3ccef4cc9dd5bc7cb559b
tkoset <cmckr88@gmail.com>
2022-01-17 21:16:59 +0300

    Update LoseModal.tsx

--- src/components/modals/LoseModal.tsx
+++ src/components/modals/LoseModal.tsx
@@ -71,7 +71,7 @@ export const LoseModal = ({ isOpen, handleClose, guesses, solution }: Props) => 
               <div className="mt-5 sm:mt-6">
                 <button
                   type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:text-sm"
+                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white sm:text-sm"
                   onClick={handleClose}
                 >
                 </button>
